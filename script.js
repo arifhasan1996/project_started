565e2c65b95ecbc351dc92e2736a47be96c932ea
Arif <Arif>
2023-10-10 11:00:58 +0600

    index.html added

---
+++ script.js
@@ -0,0 +1,34 @@
+// const panels = document.querySelectorAll('.panel');
+// panels.forEach((panel)=>{
+//     panel.addEventListener('click',() =>{
+//         removeActive()
+//         panel.classList.add('active');
+//     })
+// })
+
+// function removeActive(){
+//     panels.forEach((panel) =>(
+//         panel.addEventListener('click', () =>{
+//             panel.classList.remove('active')
+//         })
+//     ))
+// }
+
+const panels = document.querySelectorAll(".panel");
+// for add active class after click, its work perfectly
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    activeRemove();
+    panel.classList.add("active");
+  });
+});
+
+
+function activeRemove() {
+  // but when i click again it should be remove the active class but its dose not work.
+  panels.forEach((panel) => {
+    panel.addEventListener("click", () => {
+      panel.classList.remove("active");
+    });
+  });
+}
